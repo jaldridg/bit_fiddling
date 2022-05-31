63308fa4015cdbc94911eaf57434665296bfdba6
jaldridge <89814068+jaldridg@users.noreply.github.com>
2022-05-31 12:54:22 -0400

    In the middle of making things more abstract

--- main.c
+++ main.c
@@ -10,26 +10,51 @@ void test_success(unsigned int (*func_1)(unsigned int),
     }
 }
 
-void bit_test_function() {
+/*
+ * Tests the function over a range of small and large numbers
+ */
+void test_function(unsigned int (*operation_func)(unsigned int)) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < BILLION; j++) {
-            mult30(j);
+            operation_func(j);
         }
     }
 }
 
-void reg_test_function() {
+/*
+ * Tests the function over a range of small numbers
+ */
+void test_function_small(unsigned int (*operation_func)(unsigned int)) {
+    for (int i = 0; i < (1 << 17); i++) {
+        for (int j = 0; j < (1 << 16); j++) {
+            operation_func(j);
+        }
+    }
+}
+
+/*
+ * Tests the function over a range of large numbers
+ */
+void test_function_large(unsigned int (*operation_func)(unsigned int)) {
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < BILLION; j++) {
-            mult30reg(j);
+        for (int j = (1 << 16); j < (1 << 31); j++) {
+            operation_func(j);
         }
     }
 }
 
 int main() {
-    test_success(mult30, mult30reg);
-    print_runtime(bit_test_function);
-    print_runtime(reg_test_function);
+    // Function pointers to the bitwise and regular operations
+    unsigned int (*bit_op)(unsigned int) = mult30;
+    unsigned int (*reg_op)(unsigned int) = mult30reg;
+
+    // Function pointers to runtime test functions
+    void (*bit_test_func)(unsigned int (*bit_operation)(unsigned int)) = test_function(mult30);
+    void (*reg_test_func)(unsigned int (*reg_operation)(unsigned int)) = test_function(mult30reg);
+
+    test_success(bit_op, reg_op);
+    print_runtime(test_function(bit_op));
+    print_runtime(test_function(reg_op));
 
     return 0;
 }
